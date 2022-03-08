bf7020eb1ee7865963b2faf6fe29edd276c4093f
Chad Linthicum <portableanswers@gmail.com>
2022-03-08 13:38:57 -0500

    Test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
-import Table from "./Table";
+// import Table from "./Table";
+
+const persons = [
+  {
+    name: "Chad",
+    job: "Software Dev",
+  },
+  {
+    name: "Emily",
+    job: "Vet Assistant",
+  },
+  {
+    name: "Erica",
+    job: "Teacher",
+  },
+  {
+    name: "James",
+    job: "Salesman",
+  },
+];
 
 function App() {
   return (
     <div className="container">
-      <Table />
+      <div>{persons}</div>;
     </div>
   );
 }
