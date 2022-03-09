04aa217693c72b22af834cf292622b89d5182d63
Chad Linthicum <portableanswers@gmail.com>
2022-03-09 16:28:04 -0500

    Input functionality works and pushes form target value on submit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 
 function App() {
   const characters = [
     {
+      id: "001",
       name: "Charlie",
       job: "Janitor",
     },
     {
+      id: "002",
       name: "Mac",
       job: "Bouncer",
     },
     {
+      id: "003",
       name: "Dee",
       job: "Aspring actress",
     },
     {
+      id: "004",
       name: "Dennis",
       job: "Bartender",
     },
   ];
 
+  const [characterData, setCharacterData] = useState([]);
+  const [name, setName] = useState("");
+  const [job, setJob] = useState("");
+
+  const character = {
+    id: Math.floor(Math.random() * 1000),
+    name: name,
+    job: job,
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setCharacterData([...characterData, character]); //spread operator
+  }
+
   return (
     <div className="container">
-      <Table characterData={characters} />
+      <form onSubmit={handleSubmit}>
+        {" "}
+        Name
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        Job
+        <input
+          type="text"
+          onChange={(e) => setJob(e.target.value)}
+          value={job}
+        />
+        <br />
+        <input type="submit" />
+      </form>
+      <Table characterProps={characterData} />
     </div>
   );
 }
--- src/Table.js
+++ src/Table.js
@@ -1,26 +1,25 @@
 import React from "react";
 
-const TableHeader = () => {
-  return (
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Job</th>
-      </tr>
-    </thead>
-  );
-};
-
-const TableBody = () => {
-  return <tbody />;
-};
-
-const Table = () => {
-  const { characterData } = this.props;
+const Table = ({ characterProps }) => {
   return (
     <table>
-      <TableHeader />
-      <TableBody characterData={characterData} />
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+      <tbody>
+        {characterProps.map((character) => {
+          //character can be named whatever
+          return (
+            <tr key={character.id}>
+              <td>{character.name}</td>
+              <td>{character.job}</td>
+            </tr>
+          );
+        })}
+      </tbody>
     </table>
   );
 };
