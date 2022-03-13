4ffbaa0e9249e9d6e5c3d7fb222fd57d9083b9ce
Chad Linthicum <portableanswers@gmail.com>
2022-03-12 21:58:34 -0500

    Removed navbar classname and used just nav in css file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,10 @@ import stockImage from "../assets/stockImage.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <img src={stockImage} width="75" alt="" />
-        <h1>Expense Tracker</h1>
-      </nav>
-    </div>
+    <nav>
+      <img src={stockImage} width="75" alt="" />
+      <h1>Expense Tracker</h1>
+    </nav>
   );
 };
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Table = ({ props }) => {
   return (
-    <div>
+    <div className="table-expenses">
       <br />
       <table>
         <thead>
